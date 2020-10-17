54f8363648e85ff8ff00a206b430219c3cffbd8c
Guille36GM <gguette@unicesar.edu.co>
2020-10-17 12:37:17 -0500

    first commit

---
+++ Entity/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Persona
+    {
+        public string Identificacion { get; set; } 
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public string Departamento{ get; set; }
+        public string Ciudad { get; set; }    
+        public string Tipo { get; set; }    
+        public decimal Valor { get; set; }
+        public string Fecha { get; set; }
+    }
+}
